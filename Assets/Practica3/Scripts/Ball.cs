be88b70217ac79d55f65255b1c53e07418a34b50
FallingYoali <57368325+FallingYoali@users.noreply.github.com>
2020-02-24 19:37:29 -0600

    Practica3
    Bowling Ball and Pins

---
+++ Assets/Practica3/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    public TMPro.TMP_InputField force;
+    int distance = 12;
+    float energy = 0;
+    public TMPro.TMP_Text energy_answer;
+
+
+    public void Calculate()
+    {
+        energy = float.Parse(force.text);
+        energy = energy * distance;
+        print("Energy: " + energy);
+        energy_answer.text = energy.ToString();
+
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(float.Parse(force.text), 0f, 0f));
+    }
+}
