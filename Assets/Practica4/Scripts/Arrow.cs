a2a34a83d90ac423f05d0d64caa3a54a5fe164a5
FallingYoali <57368325+FallingYoali@users.noreply.github.com>
2020-02-26 19:46:47 -0600

    Practica 4
    Codigo + fisica sin rb.

---
+++ Assets/Practica4/Scripts/Arrow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{ 
+    float mass = 3;
+    float gravity = 9.8f;
+    Vector3 weight = new Vector3(0, 0, 0);
+    Vector3 force = new Vector3(300, 0, 0);
+    Vector3 velocity = new Vector3(0,0,0);
+    bool shoot;
+
+    //F = mv
+    //v = F/m
+
+    void Start()
+    {
+        shoot = true;
+        weight.y = mass * gravity;
+    }
+
+    void FixedUpdate()
+    {
+        if(shoot == true){
+            velocity = force / mass - weight;
+            print("Test" + velocity);
+            this.transform.Translate(velocity * Time.deltaTime);
+        }
+    }
+
+    public void Shoot()
+    {
+        print("Here!");
+        shoot = true;
+    }
+}
