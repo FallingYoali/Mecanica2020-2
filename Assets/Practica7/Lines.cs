ec547538b522e8118822108680f891ad4e2007ae
FallingYoali <57368325+FallingYoali@users.noreply.github.com>
2020-03-11 14:33:52 -0600

    Practica7
    Need to fix the error. Parabola is fine at the end but the first two points are wrong WROOONG.

---
+++ Assets/Practica7/Lines.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lines : MonoBehaviour
+{
+
+    //Line
+    GameObject myLine;
+    LineRenderer lr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Starting Line
+        myLine = new GameObject();
+        myLine.transform.position = new Vector3(0,0,0);
+        myLine.AddComponent<LineRenderer>();
+        lr = myLine.GetComponent<LineRenderer>();
+        Color color = new Color(0, 0, 0, 1);
+        lr.startColor = color;
+        lr.endColor = color;
+        lr.endWidth = 0.1f;
+        lr.startWidth = 0.1f;
+        //lr.SetPosition(0, initial_position);
+        //lr.SetPosition(1, initial_position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Practica7/ParabolicLine.cs
+++ Assets/Practica7/ParabolicLine.cs
@@ -28,12 +28,21 @@ public class ParabolicLine : MonoBehaviour
     //Variables to move the rocket
     Vector3 initial_position;
     Vector3 midway_position;
+    Vector3 current_position;
     Vector3 final_position;
     float t = 0.0f;
     bool takeOff = false;
     bool goingUp = false;
     bool goingDown = false;
-   
+
+
+    //Line
+    LineRenderer lr;
+    Vector3 cur_pos;
+    Vector3 previous_pos;
+    public GameObject[] myLine;
+
+
 
     public void ParabolicThrow()
     {
@@ -77,6 +86,36 @@ public class ParabolicLine : MonoBehaviour
         final_position.x += mid_x;  // x_distance;
         final_position.y += y_distance;
         midway_position = final_position;
+
+
+        previous_pos = refresh_position;
+        cur_pos = refresh_position;
+        int place = 0;
+        for(float i = 0; i < time; i += 0.5f, place++)
+        {
+            float y = (v_initial * Mathf.Sin(degree) * i) - (0.5f * gravity * i * i);
+            float x = v_initial * Mathf.Cos(degree) * i;
+            cur_pos = new Vector3(x, y, refresh_position.z);
+
+
+            //Create new line
+            Debug.Log("current" + cur_pos);
+            Debug.Log("prev: " + previous_pos);
+            myLine[place].transform.position = previous_pos;
+            lr = myLine[place].GetComponent<LineRenderer>();
+            Color color = new Color(0, 0, 0, 1);
+            lr.startColor = color;
+            lr.endColor = color;
+            lr.endWidth = 0.5f;
+            lr.startWidth = 0.5f;
+
+            lr.SetPosition(0, previous_pos);
+            lr.SetPosition(1, cur_pos);
+
+            previous_pos = cur_pos;
+        }
+
+
     }
 
     void Start()
@@ -100,6 +139,7 @@ public class ParabolicLine : MonoBehaviour
                 final_position.x += x_distance;
                 initial_position = midway_position;
                 t = 0.0f;
+                
                 if (goingDown)
                 {
                     goingDown = false;
