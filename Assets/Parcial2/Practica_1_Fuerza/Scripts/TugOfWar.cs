bd7cab0544002635bbee0a173529e6647a32b64b
FallingYoali <57368325+FallingYoali@users.noreply.github.com>
2020-03-25 15:56:46 -0600

    Practica1 Tug Of War
    Should work but can't fully test it because it freezes my laptop :c

---
+++ Assets/Parcial2/Practica_1_Fuerza/Scripts/TugOfWar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TugOfWar : MonoBehaviour
+{
+    int girl_Force;
+    int boy_Force;
+
+    public TMPro.TMP_Text girlForce;
+    public TMPro.TMP_Text boyForce;
+
+    void Start()
+    {
+        girl_Force = 0;
+        boy_Force = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Mouse Pressed!" + transform.position.x + " vs " + Input.mousePosition.x);
+            if(Input.mousePosition.x < 278)
+            {
+                girl_Force++;
+                girlForce.text = girl_Force.ToString();
+                this.transform.GetChild(0).transform.Translate(Vector3.left * 3 * Time.deltaTime);
+                Debug.Log("Girl!");
+            }
+            else if (Input.mousePosition.x >= 278)
+            {
+                boy_Force++;
+                boyForce.text = boy_Force.ToString();
+                this.transform.GetChild(0).transform.Translate(Vector3.right * 5 * Time.deltaTime);
+                Debug.Log("Boy!");
+            }  
+        }
+    }
+}
