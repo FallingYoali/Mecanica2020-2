7b8dbdee32fd971e13c8d21806fc2893a4c23a07
FallingYoali <57368325+FallingYoali@users.noreply.github.com>
2020-04-20 20:25:48 -0500

    Start Practice 4
    Plataformas Inclinadas

---
+++ Assets/Parcial2/Practica_4_PlanoInclinado/BallBounce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+    public Rigidbody rb;
+    void OnCollisionEnter(Collision col)
+    {
+        
+        if (col.gameObject.tag == "Bouncer")
+        {
+            //rb.AddForce(80, 0, 0, ForceMode.VelocityChange = 80);
+            rb.AddForce(80, 0, 0, ForceMode.Impulse);
+            Debug.Log("In collision!");
+
+        }
+            
+    }
+}
